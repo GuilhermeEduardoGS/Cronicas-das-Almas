f2a7c67445f52363c9d15034c379a139227aed0c
Guilherme <guilherme.gsouza@sptech.school>
2024-11-09 17:27:17 -0300

    Validações 100%

--- js/validacao.js
+++ js/validacao.js
@@ -1,15 +1,20 @@
     function valUser (){
         var user = ipt_nome.value;
-        var mensagem = ""
+        var mensagem = "";
+        var valido = true;
 
-        if(user = ''){
+        ipt_nome.style.borderColor = ""
+        msg_valUser.innerHTML = "";
+
+        if(user == ""){
             mensagem += `O campo Username não pode ser vazio.`
             ipt_nome.style.borderColor = "red"
+            valido = false
         }
 
         msg_valUser.innerHTML = mensagem;
 
-        return true
+        return valido
     }
 
     function valEmail (){
@@ -17,65 +22,78 @@
         var arroba = email.includes("@");
         var ponto = email.includes(".");
         var mensagem = "";
-        valEmail = false
+        var valido = true
         
+        ipt_email.style.borderColor = ""
+
         if(email == "" ){
-            mensagem += `O campo Email não pode ser vazio.`
+            mensagem += `O campo Email não pode ser vazio.<br>`
             ipt_email.style.borderColor = "red"
+            valido = false;
         }
 
         if(!arroba){
             mensagem += `O email precisa conter arroba (@). Ex.: fulano@email.com <br>`
             ipt_email.style.borderColor = "red"
+            valido = false;
         }
 
         if(!ponto){
             mensagem += `O email precisa conter ponto (.). Ex.: fulano@email.com <br>`
             ipt_email.style.borderColor = "red"
+            valido = false;
         }
 
         msg_valEmail.innerHTML = mensagem;
 
-        return true
+        return valido
 
     }
 
     function valConfEmail(){
+        var email = ipt_email.value;
         var confemail = ipt_confemail.value;
-        valConfEmail = false
+        var mensagem = ""
+        var valido = true
+
+        ipt_confemail.style.borderColor = ""
 
         if(confemail == ""){
             mensagem += `O campo Confirmar Email não pode ser vazio.`
-            ipt_email.style.borderColor = "red"
+            ipt_confemail.style.borderColor = "red"
+            valido = false
         }
 
         if(email != confemail){
             mensagem += `Os emails não coincidem.<br>`
-            ipt_email.style.borderColor = "red"
+            ipt_confemail.style.borderColor = "red"
+            valido = false
         }
         
-        return true
+        return valido
     }
 
     function valTelefone (){
-        var telefone = Number(ipt_telefone.value)
+        var telefone = ipt_telefone.value
         var tamanho = telefone.length;
         var mensagem = "";
-        valTelefone = false
+        var valido = true
 
-        if(!isNaN){
-            mensagem += `O campo deve ser preenchido apenas por números.`
+        if(isNaN(telefone) || telefone == ""){
+            mensagem += `O campo deve ser preenchido apenas por números.<br>`
             ipt_telefone.style.borderColor = "red"
+            valido = false
         }
 
         if(tamanho < 11){
-            mensagem += `O telefone deve ter no minímo 11 caracteres!`
+            mensagem += `O telefone deve ter no minímo 11 caracteres!<br>`
             ipt_telefone.style.borderColor = "red"
+            valido = false
         }
 
         msg_valTelefone.innerHTML = mensagem;
 
-        return true
+        return valido
     }
 
     function valSenha (){
@@ -87,65 +105,83 @@
         var minus = /[a-z]/.test(senha);
         var maius = /[A-Z]/.test(senha);
         var mensagem = "";
-        valSenha = false
+        var valido = true
         
         if(senha == ""){
             mensagem += `A senha não pode ser vazia.<br>`
             ipt_senha.style.borderColor = "red"
+            valido = false
         }
 
         if(tamanho < 8 || tamanho > 25){
             mensagem += `A senha deve conter entre 8 a 25 caracteres.<br>`
             ipt_senha.style.borderColor = "red"
+            valido = false
         }
         
         if(!esp){
             mensagem += `A senha deve conter pelo menos um caractere especial. Ex.: "!", "@", "#", "$", "%", "&"<br>`
             ipt_senha.style.borderColor = "red"
+            valido = false
         }
         
         if(!num){
             mensagem += `A senha de conter pelo menos um número.<br>`
             ipt_senha.style.borderColor = "red"
+            valido = false
         }
         
         if(!minus){
             mensagem += `A senha deve conter pelo menos uma letra minúscula.<br>`
             ipt_senha.style.borderColor = "red"
+            valido = false
         }
         
         if(!maius){
             mensagem += `A senha deve conter pelo menos uma letra maiúscula.<br>`
             ipt_senha.style.borderColor = "red"
+            valido = false
         }
 
-        msg_valSenha.innerHTML = mensagem;
+        msg_valConfSenha.innerHTML = mensagem;
 
-        return true
+        return valido
     }
 
     function valConfSenha (){
+        var senha = ipt_senha.value
         var confsenha = ipt_confsenha.value        
         var mensagem = "";
+        var valido = true
+
 
         if(confsenha == ""){
             mensagem += `O campo Confirmar Senha não pode ser vazio.<br>`
             ipt_confsenha.style.borderColor = "red"
+            valido = false
         }
 
         if(senha != confsenha){
             mensagem += `As senhas devem ser iguais.<br>`
             ipt_confsenha.style.borderColor = "red"
-        }   
+            valido = false
+        }
+        
+        msg_valSenha.innerHTML = mensagem;
 
+        return valido
     }
 
     function cadastrar(){
-
         var cadastro = true;
-
-        if(valEmail() || valSenha == "" || valTelefone == ""){
-            cadastro = false
+        
+        if(valEmail() === false || valSenha() === false || valTelefone() === false || valConfEmail() === false || valConfSenha() === false){
+            cadastro = false;
         }
-  
+        if(cadastro){
+            alert("Cadastro feito com sucesso")
+        }
+        
+        return cadastro;
+
     }
