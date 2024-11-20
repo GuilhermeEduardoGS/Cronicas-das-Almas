6a113d4ad03c0e2f5ad9644602024d2161ee75eb
Guilherme <guilherme.gsouza@sptech.school>
2024-11-19 22:17:01 -0300

    Perfil 90%

--- public/js/perfil.js
+++ public/js/perfil.js
@@ -1,8 +1,7 @@
 function editar_username(){
     var aparecer = document.getElementById('ipt_editarNome').style.display;
 
-    if(aparecer == 'none'){
-    
+    if(aparecer == 'none' || aparecer == ""){
         document.getElementById('ipt_editarNome').style.display = 'block';
         document.getElementById('id_botaoEditar').style.display = 'block';
         document.getElementById('id_botaoEditarNome').style.display = 'none';
@@ -15,6 +14,7 @@ function editar_username(){
         document.getElementById('ipt_editarNome').style.display = 'none';
         document.getElementById('id_botaoEditar').style.display = 'none';
         document.getElementById('id_botaoVoltar').style.display = 'none';
+        document.getElementById('id_botaoEditarNome').style.display = 'block';
         // document.getElementById('ipt_TeditarNome').style.display = 'none';
         // document.getElementById('id_editarEmail').style.display = 'block';
         // document.getElementById('id_editarSenha').style.display = 'block';
@@ -26,7 +26,17 @@ function editar_username(){
 function voltar(){
     var aparecer = document.getElementById('ipt_editarNome').style.display;
 
-    if(aparecer == 'block'){
-    }
+     if(aparecer === 'none' || aparecer === ""){
+         document.getElementById('ipt_editarNome').style.display = 'block';
+         document.getElementById('id_botaoEditar').style.display = 'block';
+         document.getElementById('id_botaoEditarNome').style.display = 'none';
+         document.getElementById('id_botaoVoltar').style.display = 'block';
+        }
+        else{
+        document.getElementById('ipt_editarNome').style.display = 'none';
+        document.getElementById('id_botaoEditar').style.display = 'none';
+        document.getElementById('id_botaoVoltar').style.display = 'none';
+        document.getElementById('id_botaoEditarNome').style.display = 'block';
+     }
 
 }
