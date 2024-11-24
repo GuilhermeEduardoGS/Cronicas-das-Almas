c20db038c338e924ff8224a2eb91397dc27bfc8e
GuilhermeEduardoGS <guilherme.gsouza@sptech.school>
2024-11-24 18:07:36 -0300

    Dashboard pronta

--- public/js/login.js
+++ public/js/login.js
@@ -6,13 +6,13 @@ window.onload = function logado(){
 
 function verificar(){
     if(logado != undefined){
-        document.getElementById(id="botao_acessar_perfil").style.display = 'block';
+        // document.getElementById(id="botao_acessar_perfil").style.display = 'block';
         document.getElementById(id="botao_acessar_quiz").style.display = 'block';
         document.getElementById(id="botao_acessar_login").style.display = 'none';
         document.getElementById(id="botao_acessar_sair").style.display = 'block';
     }
     else{
-        document.getElementById(id="botao_acessar_perfil").style.display = 'none';
+        // document.getElementById(id="botao_acessar_perfil").style.display = 'none';
         document.getElementById(id="botao_acessar_quiz").style.display = 'none';
         document.getElementById(id="botao_acessar_sair").style.display = 'none';
         document.getElementById(id="botao_acessar_login").style.display = 'block';
@@ -27,7 +27,7 @@ function deslogar(){
         sessionStorage.removeItem('ID_USUARIO')
         sessionStorage.removeItem('NOME_USUARIO')
 
-        document.getElementById(id="botao_acessar_perfil").style.display = 'none';
+        // document.getElementById(id="botao_acessar_perfil").style.display = 'none';
         document.getElementById(id="botao_acessar_quiz").style.display = 'none';
         document.getElementById(id="botao_acessar_sair").style.display = 'none';
         document.getElementById(id="botao_acessar_login").style.display = 'block';
